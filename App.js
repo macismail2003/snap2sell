9f51aa4f14d49d861a55221ab4cf6a20290a03e6
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2021-01-03 13:27:55 +0530

    app.js update

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react";
 //import AccountScreen from "./app/screens/AccountScreen";
 //import MessagesScreen from "./app/screens/MessagesScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+// import LoginScreen from "./app/screens/LoginScreen";
+// import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 
