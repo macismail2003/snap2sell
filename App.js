f0ca74493046c4da1db19a476197083fabb7ae66
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2021-01-08 19:42:10 +0530

    .

--- App.js
+++ App.js
@@ -4,15 +4,20 @@ import React from "react";
 //import LoginScreen from "./app/screens/LoginScreen";
 //import WelcomeScreen from "./app/screens/WelcomeScreen";
 //import ListingEditScreen from "./app/screens/ListingEditScreen";
-import { Text } from 'react-native';
+import { Button, Text } from 'react-native';
 import Screen from "./app/components/Screen";
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native'
 
+//import { MaterialCommunityIcons } from " @expo/vector-icons";
 
-const samu = () => (
+const samu = ({navigation}) => (
   <Screen>
     <Text>samu</Text>
+    <Button
+      title="Ismail"
+      onPress={() => navigation.navigate("ismail")} />
   </Screen>
 );
 
@@ -24,17 +29,35 @@ const ismail = () => (
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
-  <Stack.Navigator initialRouteName="ismail">
-    <Stack.Screen name = "samu" component={samu}/>
+  <Stack.Navigator initialRouteName="samu">
+    <Stack.Screen
+     name = "samu"
+     component={samu}
+    //  options={{
+    //    tabBarIcon: () => <MaterialCommunityIcons name="home"/>}}
+       />
     <Stack.Screen name = "ismail" component={ismail}/>
   </Stack.Navigator>
 )
 
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+  tabBarOptions={{
+    activeBackgroundColor: "tomato",
+    activeTintColor: "white",
+    inactiveBackgroundColor: "white",
+    inactiveTintColor: "black"    
+  }}>
+    <Tab.Screen name = "eli" component={ismail}/>
+    <Tab.Screen name = "moosa" component={StackNavigator}/>
+  </Tab.Navigator>
+)
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
